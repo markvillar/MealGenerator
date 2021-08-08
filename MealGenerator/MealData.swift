a19b9260bc868db5160701a03180c31252df33b3
Mark Villar <mark@markvillar.com>
2021-08-08 11:32:41 +0100

    Meal Model

---
+++ MealGenerator/MealData.swift
@@ -0,0 +1,50 @@
+//
+//  MealData.swift
+//  MealGenerator
+//
+//  Created by Mark on 08/08/2021.
+//
+
+import Foundation
+
+struct MealData: Decodable{
+    let meals: [Meal]
+}
+
+
+struct Meal: Decodable {
+    let name: String
+    let imageUrlString: String
+    let ingredients: [Ingredient]
+    let instructions: String
+}
+
+extension Meal {
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let mealDictionary = try container.decode([String: String?].self)
+        
+        var index = 1
+        var ingredients: [Ingredient] = []
+        
+        while let ingredient = mealDictionary["strIngredient\(index)"] as? String,
+              let measure = mealDictionary["strMeasure\(index)"] as? String,
+              !ingredient.isEmpty,
+              !measure.isEmpty {
+            
+            ingredients.append(.init(name: ingredient, measure: measure))
+            index += 1
+        }
+        self.ingredients = ingredients
+        name = mealDictionary["strMeal"] as? String ?? ""
+        imageUrlString = mealDictionary["strMealThumb"] as? String ?? ""
+        instructions = mealDictionary["strInstructions"] as? String ?? ""
+    }
+    
+}
+
+struct Ingredient: Decodable {
+    let name: String
+    let measure: String
+}
