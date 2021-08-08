9358f69c5a5bebbcb691a8e7c7e1c72e291a3455
Mark Villar <mark@markvillar.com>
2021-08-08 11:18:10 +0100

    Initial Commit

---
+++ MealGenerator/MealGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealGeneratorApp.swift
+//  MealGenerator
+//
+//  Created by Mark on 08/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MealGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
